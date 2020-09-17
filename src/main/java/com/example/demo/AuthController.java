69c0e007fd655542f53aaed72793139d146f0478
dev <rinsoftware@mail.ru>
2020-09-17 14:15:19 +0600

    Create AuthController and TasksController

---
+++ src/main/java/com/example/demo/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    //signin
+    //@PostMapping
+
+
+
+    //signup
+    //@PostMapping
+
+
+    //profile
+    //@GetMapping
+
+
+}
---
+++ src/main/java/com/example/demo/TasksController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class TasksController {
+
+    //create
+    //@PostMapping
+
+    //getTasks
+    //@GetMapping
+
+
+//**************************************************
+    //getTask(id)
+    //@GetMapping
+
+    //updateTask(id)
+    //Path
+
+    //deleteTask(id)
+    //Delete
+
+
+}
